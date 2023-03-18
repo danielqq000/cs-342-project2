957491fe2ada9ec9694315199b43d656c9c6882f
danielqq000 <danielqq000@yahoo.com>
2023-03-18 13:32:43 -0500

    Files merged

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -7,51 +7,102 @@
 
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 public class Game extends Application {
 
 
-	public void start(Stage primaryStage) {
+    @Override
+    public void start(Stage primaryStage) {
 
-		// Create welcome screen
-		BorderPane root = new BorderPane();
-		Button startButton = new Button("Start Playing");
+        // title settings
+        Label titleLabel = new Label("KENO!");
+        titleLabel.setStyle("-fx-font-size: 100px; -fx-font-weight: bold;");
+        titleLabel.setAlignment(Pos.CENTER);
 
-		// Create menu bar
-		GameMenu menu = new GameMenu();
-		MenuBar menuBar = menu.getMenuBar();
-		VBox vbox = new VBox();
-		vbox.getChildren().addAll(menuBar);
+        // content label
+        Label contentLabel = new Label();
+        contentLabel.setAlignment(Pos.CENTER);
+        contentLabel.setPadding(new Insets(10, 10, 10, 10));
+        contentLabel.setWrapText(true);
+        contentLabel.setMaxWidth(500);
 
-		// Create JavaFXTemplate aninmation
-		JavaFXTemplate fxAnimation = new JavaFXTemplate();
-		BorderPane animationPane = fxAnimation.getAnimationPane();
-		animationPane.getChildren().addAll(startButton);
+        // Create welcome screen
+        BorderPane root = new BorderPane();
+        Button startButton = new Button("Start Playing");
+        startButton.setOnAction(e -> {
+            startGame(primaryStage, root);
+        });
 
-		// set up border pane
-		root.setTop(vbox);
-		root.setCenter(animationPane);
+        // Create menu bar
+        GameMenu menu = new GameMenu();
+        MenuBar menuBar = menu.getMenuBar();
 
-		// Create scene and show stage
-		Scene scene = new Scene(root, 700, 700);
-		primaryStage.setTitle("Game");
-		primaryStage.setScene(scene);
-		primaryStage.show();
+        // Create JavaFXTemplate animation
+        JavaFXTemplate fxAnimation = new JavaFXTemplate();
+        BorderPane animationPane = fxAnimation.getAnimationPane();
+        animationPane.getChildren().addAll(startButton);
 
-	}
+        // create the main layout with the menu bar and start button
+        VBox layout = new VBox();
+        layout.setAlignment(Pos.CENTER);
+        layout.setSpacing(20);
+        layout.getChildren().addAll(menuBar, titleLabel, contentLabel, startButton);
+        root.setTop(layout);
+        root.setCenter(animationPane);
 
-	//Main Function
-	public static void main(String[] args) {
-		System.out.println("***Program Starts***");
-		launch(args);
-	}
+        // Create scene and show stage
+        Scene scene = new Scene(root, 700, 700);
+        primaryStage.setTitle("Keno");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public void startGame(Stage primaryStage, BorderPane root) {
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10));
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
+
+        EventHandler<ActionEvent> gridHandler = new EventHandler<>() {
+
+            public void handle(ActionEvent e) {
+                Button button = (Button)e.getSource();
+                button.setDisable(true);
+            }
+        };
+
+        int count = 1;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 10; j++) {
+                Button button = new Button(Integer.toString(count));
+                button.setPrefSize(50, 50);
+                button.setOnAction(gridHandler);
+                button.setDisable(true);
+                gridPane.add(button, j, i);
+                count++;
+            }
+        }
+
+        // replace the center of the root pane with the gridPane
+        root.setCenter(gridPane);
+    }
+
+    //Main Function
+    public static void main(String[] args) {
+        System.out.println("***Program Starts***");
+        launch(args);
+    }
 }
-
--- src/main/java/GameMenu.java
+++ src/main/java/GameMenu.java
@@ -59,7 +59,13 @@ public class GameMenu{
 		Alert alert = new Alert(Alert.AlertType.INFORMATION);
 		alert.setTitle("Rules of the Game");
 		alert.setHeaderText("***Rules***");
-		alert.setContentText("These are the rules of the game...");
+		String rulesText = "These are the rules of the game..." +
+			"1. Choose number of spots (1, 4, 8, 10)\n" +
+			"2. Select numbers OR auto select\n" +
+			"3. Choose number of draws\n" +
+			"4. Click 'PLAY'\n" +
+			"5. The more you match, the more you win";
+		alert.setContentText(rulesText);
 		alert.showAndWait();
 	}
 
@@ -68,7 +74,25 @@ public class GameMenu{
 		Alert alert = new Alert(Alert.AlertType.INFORMATION);
 		alert.setTitle("Odds of the Game");
 		alert.setHeaderText(null);
-		alert.setContentText("These are the odds of the game...");
+		String oddsText = "Here are the odds of winning..." +
+			"1 Number\t1\t1 in 4\n" +
+			"\t\t\t0\t1 in 3.33\n" +
+			"4 Numbers\t4\t1 in 326\n" +
+			"\t\t\t3\t1 in 23.12\n" +
+			"\t\t\t2\t1 in 4.70\n" +
+			"8 Numbers\t8\t1 in 230,114\n" +
+			"\t\t\t7\t1 in 6,232.27\n" +
+			"\t\t\t6\t1 in 422.53\n" +
+			"\t\t\t5\t1 in 54.64\n" +
+			"\t\t\t4\t1 in 12.27\n" +
+			"10 Numbers\t10\t1 in 8,911,711\n" +
+			"\t\t\t9\t1 in 163,381\n" +
+			"\t\t\t8\t1 in 7,384\n" +
+			"\t\t\t7\t1 in 621\n" +
+			"\t\t\t6\t1 in 87.11\n" +
+			"\t\t\t5\t1 in 19.44\n" +
+			"\t\t\t0\t1 in 21.84";
+			alert.setContentText(oddsText);
 		alert.showAndWait();
 	}
 
